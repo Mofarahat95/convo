c0cf11b6105de780676372eec467dec4aba5e8b0
Kerolos Emad <keroemad723@gmail.com>
2025-02-11 00:17:44 +0200

    statr fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:convo/config/routes_manager/routes_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-void main(){
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Convo());
 }
 class Convo extends StatelessWidget {
