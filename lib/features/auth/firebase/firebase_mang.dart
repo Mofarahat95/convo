173110b1a9968d033b52bf0566efa4b9543a3585
Kerolos Emad <keroemad723@gmail.com>
2025-02-13 02:57:26 +0200

    login with googel create function

--- lib/features/auth/firebase/firebase_mang.dart
+++ lib/features/auth/firebase/firebase_mang.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class firbaseeMang {
   static Future<void> createAccount(String email, String password) async {
     try {
       final credential =
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
@@ -26,8 +27,22 @@ class firbaseeMang {
           .signInWithEmailAndPassword(email: email, password: password);
       onSuccess();
     } on FirebaseAuthException catch (e) {
-        onError(e.message);
-      }
+      onError(e.message);
     }
   }
 
+  // Google Sign In
+  signInWithGoogle() async {
+// begin interactive sign in process
+final GoogleSignInAccount? gUser= await GoogleSignIn().signIn();
+// obtaiin auth details from request
+final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+
+// create a new credential for user
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken, idToken: gAuth.idToken,);
+// finally, lets sign in
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
