4644443a3a847c172a9f462c288dae5294226f49
Mohamed Farahat <121612492+Mofarahat95@users.noreply.github.com>
2025-02-14 16:19:10 +0200

    Update routes.dart

--- lib/config/routes_manager/routes.dart
+++ lib/config/routes_manager/routes.dart
@@ -1,8 +1,8 @@
 abstract class AppRoutes{
-  static const String splashRoute='/';
+  static const String splashRoute='';
   static const String homeRoute='/home';
   static const String signUpRoute='';
-  static const String loginRoute='/';
+  static const String loginRoute='/signUp';
   static const String settingsRoute='/Settings';
   static const String Home='/home_screen.dart';
 }
